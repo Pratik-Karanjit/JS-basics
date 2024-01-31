2e09e83a374e2b70a3351f22aeb34d8345e2ce2d
Pratik Karanjit <pratikkaranjit@gmail.com>
2024-01-31 10:35:20 +0545

    Learn API hit using async await

---
+++ practice/src/AllPractice/ApiUsingAsyncAwait.js
@@ -0,0 +1,34 @@
+// Question is to hit an api from json placeholder that gets posts information in console with extracting just 10 of the userIds from the array of objects
+
+import axios from "axios";
+import React from "react";
+
+const ApiUsingAsyncAwait = () => {
+  let ApiCall = async () => {
+    try {
+      let response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      const userIds = response.data
+        .slice(0, 10)
+        .map((individualPosts) => individualPosts.userId);
+      console.log(userIds);
+    } catch (error) {
+      console.log("unable to get JSON data.", error);
+    }
+  };
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          ApiCall();
+        }}
+      >
+        Click to get posts
+      </button>
+    </div>
+  );
+};
+
+export default ApiUsingAsyncAwait;
