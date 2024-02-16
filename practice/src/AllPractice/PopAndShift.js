07a1e676efd082fee3bdf2de8e18f4805f7bbab8
Pratik Karanjit <pratikkaranjit@gmail.com>
2024-02-16 17:33:05 +0545

    Add interview questions

---
+++ practice/src/AllPractice/PopAndShift.js
@@ -0,0 +1,10 @@
+//Pop removes the last element from the array and returns it
+let myArray = ["a", "b", "c", "d", "e", "f"];
+
+console.log(myArray.pop());
+console.log(myArray);
+
+//Shift removes the first element from the array and returns it
+let mySpace = ["m", "n", "o", "p"];
+console.log(mySpace.shift());
+console.log(mySpace);
---
+++ practice/src/AllPractice/PushAndUnShift.js
@@ -0,0 +1,15 @@
+// The push() method adds new items to the end of an array.
+
+// The push() method changes the length of the array.
+
+// The push() method returns the new length.
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.push("Kiwi")); //This returns length of the pushed array
+console.log(fruits);
+
+//Unshift adds new element to the beginning of the array and returns the length
+
+const fruits_ = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits_.unshift("Lemon", "Pineapple"));
+console.log(fruits_);
