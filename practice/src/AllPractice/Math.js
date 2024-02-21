cf74b18936077802f6ca6f065cb4e4d358a82aca
Pratik Karanjit <pratikkaranjit@gmail.com>
2024-02-21 17:52:14 +0545

    add interview questions

---
+++ practice/src/AllPractice/Math.js
@@ -0,0 +1,5 @@
+console.log(Math.random()); //Gives random number between 0 and 1
+
+let floatValue = 7.1;
+console.log(Math.floor(floatValue)); //Ignores the decimal point and anything after it.
+console.log(Math.ceil(floatValue)); //Rounds off the decimal point.
