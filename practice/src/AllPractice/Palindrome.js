604c59984a578607829d4d54af7ddceb1cf67996
Pratik Karanjit <pratikkaranjit@gmail.com>
2024-02-25 21:51:19 +0545

    add comments

--- practice/src/AllPractice/Palindrome.js
+++ practice/src/AllPractice/Palindrome.js
@@ -1,5 +1,9 @@
 let word = "No lemon, no Melon";
 
+//regex pattern to remove it and replace it with no space
+//change all to lower case
+//split it to take it into array, reverse and return it in string
+//check the two variables
 let checker = (input) => {
   let replaceWord = input.replace(/[\W_]/g, "");
   let lowerWord = replaceWord.toLowerCase();
