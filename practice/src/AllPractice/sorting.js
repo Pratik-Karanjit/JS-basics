6642275f4839eb5439e6535f746043813a4d390b
Pratik Karanjit <pratikkaranjit@gmail.com>
2024-02-28 15:02:35 +0545

    add comments

--- practice/src/AllPractice/sorting.js
+++ practice/src/AllPractice/sorting.js
@@ -7,7 +7,7 @@ let myFunction = (input) => {
 
 console.log(myFunction(myArr));
 
-//Descending order
+//Descending order with variable initialized
 let myArray = [1, 2, 3, 4, 5];
 
 let myFunc = (input) => {
