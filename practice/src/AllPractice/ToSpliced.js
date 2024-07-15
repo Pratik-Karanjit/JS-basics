c81e53afa8696ac5cd9a834f8550ecf40a421486
Pratik Karanjit <pratikkaranjit@gmail.com>
2024-07-15 18:26:25 +0545

    add toSpliced method of Array in JS

---
+++ practice/src/AllPractice/ToSpliced.js
@@ -0,0 +1,6 @@
+//The ToSpliced method returns the remaining of the array that is left after slicing.
+
+const months = ["Jan", "Feb", "Mar", "Apr"];
+const spliced = months.toSpliced(0, 2);
+
+console.log(spliced);
